b16bd8e37f08a4e97aabef2db6e2fc83e277b42c
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-17 22:32:20 -0700

    add solution to countElements

--- phaseOne/arrays-hashing/mini-challenges #1/frequency-counting/countElements.js
+++ phaseOne/arrays-hashing/mini-challenges #1/frequency-counting/countElements.js
@@ -6,3 +6,21 @@
 // Frequencies: 1:3, 2:2, 3:1, 4:4
 // Numbers with frequency > 2: 1 (freq 3), 4 (freq 4)
 // Result: 2 (because there are 2 such distinct numbers)
+
+function countElements (nums, k){
+    const map = new Map()
+    for(let numbers of nums){
+        map.set(numbers, (map.get(numbers) || 0) + 1)
+    }
+    const arr = Array.from(map.entries())
+    let result = 0
+
+    for(let i=0; i<arr.length; i++){
+        if(arr[i][1] > k ){
+            result++
+        }
+    }
+ return result
+}
+
+console.log(countElements([1, 1, 1, 2, 2, 3, 4, 4, 4, 4], 2))
