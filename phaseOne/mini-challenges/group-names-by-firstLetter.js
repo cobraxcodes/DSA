da3833f9411c7b0dfe5ab82178e866faa1ce5a8d
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-17 17:16:18 -0700

    add answer to group by first letter in name

--- phaseOne/mini-challenges/group-names-by-firstLetter.js
+++ phaseOne/mini-challenges/group-names-by-firstLetter.js
@@ -8,3 +8,20 @@
 //   ["Chris"]
 // ]
 // Key: name[0]
+
+
+function firstLetter (str){
+    const map = new Map()
+    for(let names of str ){
+        let key = names[0]
+        if(map.has(key)){
+            map.get(key).push(names)
+        }else{
+            map.set(key, [names])
+        }
+    }
+              return Array.from(map.values()).sort()
+ 
+}
+
+console.log(firstLetter(["Anna", "Alex", "Brian", "Bella", "Chris"]))
