58eac78db58ed3419ba27b25d281768f8ce8ed55
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-17 17:42:08 -0700

    add answer to group by sorted letters

--- phaseOne/mini-challenges/group-string-by-sortedLetters.js
+++ phaseOne/mini-challenges/group-string-by-sortedLetters.js
@@ -9,3 +9,19 @@
 //   ["cat"]
 // ]
 // Key: sorted version of string → word.split('').sort().join('')
+
+
+function sorted(arr){
+    const map = new Map()
+    for(let word of arr ){
+        let key= word.split('').sort().join('')
+        if(map.has(key)){
+            map.get(key).push(word)
+        }else{
+            map.set(key, [word])
+        }
+    }
+    return Array.from(map.values())
+}
+
+console.log(sorted(["bat", "tab", "tap", "pat", "cat"]))
