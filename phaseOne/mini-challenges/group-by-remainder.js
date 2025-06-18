cd8ca4f7d6db845d421f9aa96c40ff9b1a4bdcc3
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-17 17:10:52 -0700

    add answer to group-by-remainder.js

--- phaseOne/mini-challenges/group-by-remainder.js
+++ phaseOne/mini-challenges/group-by-remainder.js
@@ -9,3 +9,19 @@
 //   [7, 10]   // remainder 1
 // ]
 // Key: num % 3
+
+
+ function remainder (arr){
+    const map = new Map()
+    for(let nums of arr ){
+        let key = nums % 3
+        if(map.has(key)){
+            map.get(key).push(nums)
+        }else{
+            map.set(key, [nums])
+        }
+    }
+    return Array.from(map.values())
+ }
+
+console.log(remainder([5, 9, 7, 2, 6, 10]))
