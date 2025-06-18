781b21b645745403b2898302f69b31eb4062bc0e
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-17 22:22:18 -0700

    add solution to allElements.js

--- phaseOne/arrays-hashing/mini-challenges #1/frequency-counting/allElements.js
+++ phaseOne/arrays-hashing/mini-challenges #1/frequency-counting/allElements.js
@@ -4,3 +4,20 @@
 // Hint: You're counting frequencies here too, but you don't need to sort. What condition would tell you if an element is not unique?
 // Example: areAllUnique([1, 2, 3, 4]) should return true.
 // Example: areAllUnique([1, 2, 3, 2]) should return false.
+
+const unique = (arr) =>{
+    let map = new Map()
+    for (let numbers of arr){
+        map.set(numbers, (map.get(numbers) || 0) + 1)
+    }
+    const newArr = Array.from(map.entries())
+    for(let i=0; i<newArr.length; i++){
+        if(newArr[i][1] !== 1){
+            return false
+        }
+    }
+    return true
+}
+
+console.log(unique([1, 2, 3, 4]))
+console.log(unique([1, 2, 3, 2]))
