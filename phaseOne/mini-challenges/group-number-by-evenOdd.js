969c6a357defa4fb449649c11cb36a465d291e98
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-17 17:38:36 -0700

    add answer to even or odd

--- phaseOne/mini-challenges/group-number-by-evenOdd.js
+++ phaseOne/mini-challenges/group-number-by-evenOdd.js
@@ -8,3 +8,18 @@
 //   [1, 3, 5]   // odd
 // ]
 // Key: num % 2 (0 = even, 1 = odd)
+
+function oddEven (arr){
+    const map = new Map()
+    for(let nums of arr){
+        let key = nums % 2
+        if(map.has(key)){
+            map.get(key).push(nums)
+        }else{
+            map.set(key, [nums])
+        }
+    }
+    return Array.from(map.values())
+}
+
+console.log(oddEven([1, 2, 3, 4, 5, 6]))
