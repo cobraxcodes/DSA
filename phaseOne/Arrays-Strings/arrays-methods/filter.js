8e8f88aa7da6c8b30ee0aa96fda538bbd62378dd
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-23 19:24:32 -0700

    feat: complete .filter() easy set

--- phaseOne/Arrays-Strings/arrays-methods/filter.js
+++ phaseOne/Arrays-Strings/arrays-methods/filter.js
@@ -1,19 +1,34 @@
 // 🟢 5 Easy Problems:
 // Filter out numbers less than 5.
 // [1, 2, 5, 7] → [5, 7]
+const nums =[1,2,5,7]
+const filtered = nums.filter(x => x >= 5)
+console.log(filtered)
 
 // Filter even numbers.
 // [1, 2, 3, 4] → [2, 4]
+const even = [1,2,3,4]
+const evenFilter = even.filter(x => x % 2 === 0)
+console.log(evenFilter)
 
 // Filter strings with length > 3.
 // ['hi', 'hello'] → ['hello']
 
+const str = ['hi', 'hello']
+const filterStr = str.filter(x => x.length > 3)
+console.log(filterStr)
+
 // Filter out falsy values.
 // [0, 1, false, 'a'] → [1, 'a']
+const values = [0, 1, false, 'a']
+const falsy = values.filter(value => !value)
+console.log(falsy)
 
 // Filter people over age 21.
 // [{name:'A', age:22}] → [{name:'A', age:22}]
-
+const obj = [{name:'A', age:22}, {name: "K", age: 20}]
+const filterB = obj.filter(x => x.age > 21)
+console.log(filterB)
 
 
 // 🟡 5 Intermediate Problems:
