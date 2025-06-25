d2acbaf9b99929b8d7af86688a5de9195a417d09
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-24 19:02:37 -0700

    feat: add .substring() solutions

--- phaseOne/Arrays-Strings/string-methods/substring.js
+++ phaseOne/Arrays-Strings/string-methods/substring.js
@@ -1,6 +1,8 @@
 // 🟢 Easy:
-// Get characters from index 0 to 4.
-
+// Get characters from index 1 to 4.
+const str = "Hello world"
+const rest = str.substring(1,5)
+console.log(rest)
 // Get characters from index 3 to the end.
 
 // Get the middle 3 characters of a string.
@@ -8,10 +10,14 @@
 // Get characters from index 1 to 5.
 
 // Get characters from index 0 to the string’s length.
+const testB = str.substring(0, str.length)
+console.log(testB)
 
 // 🟡 Intermediate:
 // Extract the username from an email before the @.
-
+const email = "testme@mail.com"
+const testC = email.split('@')[0]
+console.log(testC)
 // Extract the file extension from a filename string.
 
 // Use substring to swap two parts of a string.
