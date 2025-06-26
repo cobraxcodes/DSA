31753c74918a3da903a5650d22bcdbe05b17cc74
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-25 22:59:11 -0700

    chore: add challneges for binary search

---
+++ phaseOne/Arrays-Strings/binary-search/challenges.js
@@ -0,0 +1,14 @@
+// 🔹 Easy
+// 1️⃣ Find the index of 42 in a sorted array.
+// 2️⃣ Check if a target number exists in a sorted list.
+// 3️⃣ Return the index of the first element equal to a target.
+// 4️⃣ Return -1 if the target number doesn’t exist.
+// 5️⃣ Find the middle number in a sorted array.
+
+// 🔹 Intermediate
+// 6️⃣ Find the first occurrence of a duplicate number in a sorted array.
+// 7️⃣ Find the insert position for a number in a sorted array.
+// 8️⃣ Return the index of the smallest number greater than a target.
+// 9️⃣ Count how many times a target appears in a sorted array.
+// 🔟 Search for a number in a rotated sorted array (this one’s interview gold 💰)
+
