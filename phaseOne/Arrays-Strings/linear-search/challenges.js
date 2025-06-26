ce9003d9da8799057f356aaa752cace9d7511eef
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-25 23:00:14 -0700

    chore: add challenges for  linear search

---
+++ phaseOne/Arrays-Strings/linear-search/challenges.js
@@ -0,0 +1,6 @@
+// 🔹 Intermediate
+// 6️⃣ Find the first number greater than 50 in an array.
+// 7️⃣ Find the last occurrence of a word in an array.
+// 8️⃣ Find if two different numbers add up to a target sum (brute force linear search way).
+// 9️⃣ Return the number of elements greater than a given value.
+// 🔟 Replace the first occurrence of a word in a string array.
