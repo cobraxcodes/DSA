1e56d69d5a199e0a0045e4bfbc9f0d48304d19e7
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-25 20:17:33 -0700

    feat: add solution for .forEach()

--- phaseOne/Arrays-Strings/array-traversals/forEach.js
+++ phaseOne/Arrays-Strings/array-traversals/forEach.js
@@ -1,16 +1,47 @@
 // 1️⃣ Print the index + value of each item
-// ['a', 'b', 'c']
+const nums = ['a', 'b', 'c']
 // // Output: 0:a  1:b  2:c
+function count (nums){
+    let obj = {}
+    nums.forEach((x) =>{
+        if(obj[x]){
+            obj[x]++
+        }else{
+            obj[x] = 1
+        }
+    })
+    return obj
+}
+
+// console.log(count(nums))
 
 
 // 2️⃣ Sum the lengths of all strings in an array
 // ['hi', 'hello', 'world']  
 // // Output: 13
 
+function sum (arr){
+    const stringArray = arr.join('').split("")
+        console.log(stringArray)
+    let sumStrings = 1
+    stringArray.forEach(x => {
+        sumStrings++
+    })
+return sumStrings
+}
+
+// console.log(sum(['hi', 'hello', 'world']))
 
 // 3️⃣ Convert all strings to uppercase and push them into a new array
 // ['dog', 'cat', 'bird']  
 // // Output: ['DOG', 'CAT', 'BIRD']
+function upper(arr){
+    const oldArr = arr.join("").toUpperCase().split("")
+    const newArry = []
+    oldArr.forEach(x => newArry.push(x))
+    return newArry
+}
+// console.log(upper(['dog', 'cat', 'bird']))
 
 
 
@@ -18,8 +49,29 @@
 // ['hi', 'hello']  
 // // Output: { hi: 2, hello: 5 }
 
+function map(str){
+    let obj = new Object()
+    for(let keys of str){
+      obj[keys] = keys.length
+    }
+    return obj
+}
+
+// console.log(map(['hi', 'hello']  ))
+
 
 
 // 5️⃣ Count how many words have more than 5 letters
 // ['hi', 'hello', 'goodbye', 'yes']  
 // // Output: 1
+
+function five(arr){
+    let counter = 0
+    arr.forEach(x => {
+        if(x.length > 5){
+            counter++
+        }
+    })
+    return counter
+}
+console.log(five(['hi', 'hello', 'goodbye', 'yes', "magicmike", "watermelon"]))
